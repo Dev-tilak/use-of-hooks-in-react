af29e30619d8037f9352230d4883e64267f239f2
Tilakdhari Pandit <116021031+Tilakdhari62035@users.noreply.github.com>
2024-04-09 15:31:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from'./components/navbar';
+import MainBody from'./components/mainBody';
+// import Products from "../src/components/products"
+let App=()=>{
+  return <div>
+    <Navbar></Navbar>
+    <MainBody></MainBody>
+    
+  </div> 
+}
+export default App;
